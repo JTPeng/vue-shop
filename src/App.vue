87f1c58130d0ea7078b8fdf5099ad6b564bc1557
JTPeng <404502753@qq.com>
2019-07-15 18:00:40 +0800

    使用axios发送请求获取数据

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,34 @@
 <template>
   <div>
-    <FooterGuide v-show="$route.meta.isShowFooter"/>
+    <FooterGuide v-show="$route.meta.isShowFooter" />
     <router-view></router-view>
   </div>
 </template>
 
 <script>
 // 引入组件
-import FooterGuide from './components/FooterGuide';
+import FooterGuide from "./components/FooterGuide";
+
+// 引入api进行测试
+import { reqAddress, reqCategories, reqShops, reqUserMessages } from "./api";
 export default {
   name: "App",
   // 注册组件
   components: {
     FooterGuide
+  },
+  async mounted() {
+    const result = await reqAddress("116.36867","40.10038");
+    console.log("result :", result);
+    const result1 = await reqCategories();
+    console.log("result1 :", result1);
+    const result2 = await reqShops("116.36867","40.10038");
+    console.log('result2 :', result2);
+    const result3 = await reqUserMessages();
+    console.log('result3 :', result3);
   }
 };
 </script>
 
 <style scope>
-
 </style>
