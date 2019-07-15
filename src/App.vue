4789bb631fba884c3a3530b87ffde8bf60b609d2
JTPeng <404502753@qq.com>
2019-07-15 11:13:38 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    你好啊
+    <FooterGuide/>
+  </div>
+</template>
+
+<script>
+import FooterGuide from './components/FooterGuide/FooterGuide.vue';
+export default {
+  name: "App",
+  components: {
+    FooterGuide
+  }
+};
+</script>
+
+<style scope>
+
+</style>
